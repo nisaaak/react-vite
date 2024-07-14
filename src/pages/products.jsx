19c03902f5c2f71a2b4a71acf6c3923c3a4b3538
k nisak <khoirun13nisak@gmail.com>
2024-07-14 20:36:48 +0700

    maping

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,23 +1,28 @@
 import CardProduct from "../components/Fragments/CardProduct"
 
+const products = [
+    {
+        id: 1,
+        name: 'Boneka Baru',
+        price: 'Rp 100.000',
+        image: '/images/bear.jpg',
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex corporis unde aliquam ipsam rerum aliquid natus facere rem. Esse aperiam ad qui ipsa tenetur ex itaque culpa doloribus possimus natus.'
+    }
+]
 
 const ProductPage = () => {
     return (
         <div className="flex justify-center py-5">
-            <CardProduct>
-                <CardProduct.Header image={"/images/bear.jpg"} />
-                <CardProduct.Body title="Boneka Baru">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex corporis unde aliquam ipsam rerum aliquid natus facere rem. Esse aperiam ad qui ipsa tenetur ex itaque culpa doloribus possimus natus.
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp 100.000" />
-            </CardProduct>
-            <CardProduct>
-                <CardProduct.Header image={"/images/bear.jpg"} />
-                <CardProduct.Body title="Boneka Baru">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex corporis unde aliquam ipsam rerum aliquid natus facere rem. Esse aperiam ad qui ipsa tenetur ex itaque culpa doloribus possimus natus.
-                </CardProduct.Body>
-                <CardProduct.Footer price="Rp 100.000" />
-            </CardProduct>
+            {products.map((product) => (
+                <CardProduct>
+                    <CardProduct.Header image={product.image} />
+                    <CardProduct.Body name={product.name}>
+                        {product.description}
+                    </CardProduct.Body>
+                    <CardProduct.Footer price={product.price} />
+                </CardProduct>
+            ))
+            }
         </div>
     )
 }
