73857e210e0889973ad8f7050c7f890e9c195023
k nisak <khoirun13nisak@gmail.com>
2025-01-14 14:29:58 +0700

    Custom Hooks

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -3,12 +3,13 @@ import Button from "../components/Elements/Button"
 import CardProduct from "../components/Fragments/CardProduct"
 import { getProducts } from "../services/product.services"
 import { getUsername } from "../services/auth.services"
+import { useLogin } from "../hooks/useLogin"
 
 const ProductPage = () => {
     const [cart, setCart] = useState([]) //{id:1,qty:1}
     const [totalPrice, setTotalPrice] = useState(0)
     const [products, setProducts] = useState([])
-    const [username, setUsername] = useState('')
+    const username = useLogin()
 
     // useref => tdk langsung ter render perubahan nya
     // memanipulasi dom spt Document.getElementById()
@@ -22,16 +23,6 @@ const ProductPage = () => {
 
     useEffect(() => {
         setCart(JSON.parse(localStorage.getItem('cart')) || [])
-    }, [])
-
-    useEffect(() => {
-        const token = localStorage.getItem('token')
-        if (token) {
-            setUsername(getUsername(token))
-        } else {
-            window.location.href = "/login"
-        }
-
     }, [])
 
     useEffect(() => {
